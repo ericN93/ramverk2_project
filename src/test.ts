95126c91f399b21db87edb24fda8b699a4f0ecdf
Eric Nilsson <eric.nilsson931211@gmail.com>
2017-12-16 14:29:09 +0100

    added logic in karma

--- src/test.ts
+++ src/test.ts
@@ -26,7 +26,10 @@ getTestBed().initTestEnvironment(
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
+const context2 = require.context('../server/test', true, /\.spec\.ts$/);
+
 // And load the modules.
 context.keys().map(context);
+context.keys().map(context2);
 // Finally, start Karma to run the tests.
 __karma__.start();
